c9ac2decdfbbe023154d2a7524fcd01f40d882cf
Lenko Ivanov <lenko.ivanov117@gmail.com>
2020-10-14 16:09:00 +0000

    ubung3

--- src/exercise3/NumberCounter.java
+++ src/exercise3/NumberCounter.java
@@ -10,6 +10,7 @@ public class NumberCounter{
     int negativecounter = 0;
     double sum = 0;
     double average = 0;
+    int counter = 0;
 
     int number = input.nextInt();
 
@@ -19,14 +20,15 @@ public class NumberCounter{
       }else{
         negativecounter++;
       }
+      counter++;
       sum += number;
-      average += number;
+      average = sum;
       number=input.nextInt();
     }
 
     System.out.println("Positive numbers: " + positivecounter);
     System.out.println("Negative numbers: " + negativecounter);
     System.out.println("Sum: " + sum);
-    System.out.println("Average: " + (average/2.0));
+    System.out.println("Average: " + (average / counter) );
   }
 }
