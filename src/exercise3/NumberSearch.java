06de62d918491f098b660b8610d2a297161f5212
Lenko Ivanov <lenko.ivanov117@gmail.com>
2020-10-14 18:23:36 +0000

    ubung3

--- src/exercise3/NumberSearch.java
+++ src/exercise3/NumberSearch.java
@@ -6,11 +6,12 @@ public class NumberSearch{
      int counter = 0;
 
      for(int i = 100; i <= 1000; i++){
-       if(i % 30 == 0){
+       if(i % 30 == 0 && counter < 10){
           System.out.print(i);
           System.out.print(" ");
           counter++;
           if(counter == 10){
+            System.out.print(i);
             counter = 0;
             System.out.print("\n");
           }
