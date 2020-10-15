ea678b8836be03c065363f467f51572ba3e81164
Lenko Ivanov <lenko.ivanov117@gmail.com>
2020-10-15 06:15:21 +0000

    ubung3

---
+++ src/exercise3/Triangle.java
@@ -0,0 +1,42 @@
+package exercise3;
+
+import java.util.Scanner;
+
+public class Triangle{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+
+    double a = input.nextDouble();
+    double b = input.nextDouble();
+    double c = input.nextDouble();
+    String type = "";
+    double s = (a + b + c)/2.0;
+    double area = Math.sqrt(s * (s-a) * (s-b) * (s-c));
+    
+
+    do{
+      if(a < b + c || b < a + c || c < a + b){
+           if(a == b && b == c){
+            type = "equilateral";
+            System.out.println("Area: " + area);
+            System.out.println("Triangle: " + type);
+           }else if(a == b || b == c || a == c){
+             type = "isosceles";
+             System.out.println("Area: " + area);
+             System.out.println("Triangle: " + type);
+           }else if (a != b && b != c && a != c){
+             type = "scalene";
+             System.out.println("Area: " + area);
+             System.out.println("Triangle: " + type);
+           }
+      }else{
+        System.out.println("Values are not correct!");
+      }
+
+      a = input.nextDouble();
+      b = input.nextDouble();
+      c = input.nextDouble();
+
+    }while(a > 0 && b > 0 && c > 0);
+  }
+}
