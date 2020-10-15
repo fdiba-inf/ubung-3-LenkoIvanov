d4e14ff6409cea41f80c8fc70b582dd15bb46c98
Lenko Ivanov <lenko.ivanov117@gmail.com>
2020-10-15 05:57:05 +0000

    ubung3

--- src/exercise3/AngleCalculation.java
+++ src/exercise3/AngleCalculation.java
@@ -2,7 +2,7 @@ package exercise3;
 
 import java.util.Scanner;
 
-public class AngleCalculator{
+public class AngleCalculation{
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
 
