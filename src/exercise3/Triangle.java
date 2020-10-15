b4be7fb569e0d1007c05c2cc80c3d3742f8e38da
Lenko Ivanov <lenko.ivanov117@gmail.com>
2020-10-15 15:22:48 +0000

    ubung3

--- src/exercise3/Triangle.java
+++ src/exercise3/Triangle.java
@@ -4,38 +4,31 @@ import java.util.Scanner;
 
 public class Triangle{
   public static void main(String[] args){
-    Scanner input = new Scanner(System.in);
+    Scanner input = new Scanner (System.in);
 
     double a = input.nextDouble();
     double b = input.nextDouble();
     double c = input.nextDouble();
     String type = "";
-    double s = (a + b + c)/2.0;
-    double area = Math.sqrt(s * (s-a) * (s-b) * (s-c));
-    
 
     do{
-      if(a < b + c && b < a + c && c < a + b){
-           if(a == b && b == c && a == c){
-            type = "equilateral";
+        if(a < c + b && b < a + c && c < a + b){
+            if(a == c && b == c && a == b){
+                type = "equilateral";
+            }else if(a == c || b == c || a == b){
+              type = "isosceles";
+            }else{
+              type = "scalene";
+            }
+            double s = (a + b + c)/2.0;
+            double area = Math.sqrt(s * (s-a) * (s-b) * (s-c));
+
             System.out.println("Area: " + area);
             System.out.println("Triangle: " + type);
-           }else if(a == b || b == c || a == c){
-             type = "isosceles";
-             System.out.println("Area: " + area);
-             System.out.println("Triangle: " + type);
-           }else if (a != b && b != c && a != c){
-             type = "scalene";
-             System.out.println("Area: " + area);
-             System.out.println("Triangle: " + type);
-           }
-      }else{
-        System.out.println("Values are not correct!");
-      }
 
-      a = input.nextDouble();
-      b = input.nextDouble();
-      c = input.nextDouble();
+        }else{
+          System.out.println("Values are not correct!")
+        }
 
     }while(a > 0 && b > 0 && c > 0);
   }
